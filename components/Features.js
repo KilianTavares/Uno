3c6295c17a8ca7afde069fae7f26c07a7bf5dc52
sitelinestudios <sitelinestudios@gmail.com>
2024-10-20 22:26:57 +1100

    Uno first version

---
+++ components/Features.js
@@ -0,0 +1,209 @@
+export default function Features() {
+  return (
+    <section
+      id="features"
+      className="py-20 bg-gradient-to-t from-[#524790] to-[#8c79ed]"
+    >
+      <div className="mx-8 max-w-7xl md:mx-10 lg:mx-20 xl:mx-auto">
+        <div className="space-y-16 md:space-y-24">
+          <div className="transition duration-500 ease-in-out transform scale-100 translate-x-0 translate-y-0 opacity-100">
+            <div className="space-y-5 md:text-center">
+              <div className="inline-block rounded-lg bg-[#302953] bg-opacity-60 px-3 py-1 text-sm font-semibold text-indigo-100 hover:cursor-pointer hover:bg-opacity-40">
+                Awesome Features
+              </div>
+              <h1 className="text-3xl font-semibold text-gray-100 md:text-5xl">
+                Decked with countless features.
+              </h1>
+              <p className="text-xl text-gray-200 md:text-2xl">
+                Each feature is specifcially made for teams and aimed at making{" "}
+                <span className="font-semibold">your</span> life easier.
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 grid-rows-2 gap-8 text-lg md:grid-cols-2 lg:grid-cols-3">
+            <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
+              >
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+              </svg>
+              <h3 className="text-xl font-semibold text-white">Efficiency</h3>
+              <p className="text-gray-200">
+                A SaaS payroll solution improves efficiency by automating
+                numerous payroll functions. From calculating wages and taxes to
+                generating payslips and managing benefits, these services reduce
+                the need for manual data entry and minimise the risk of human
+                error. This automation not only saves time but also ensures
+                accuracy, enabling fast, effortless payroll processing.
+              </p>
+            </div>
+            <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
+              >
+                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+              </svg>
+              <h3 className="text-xl font-semibold text-white">
+                The latest technologies
+              </h3>
+              <p className="text-gray-200">
+                By outsourcing payroll processes to a SaaS provider, companies
+                can free up valuable time and resources to allocate to other
+                critical aspects of business. Instead of dedicating hours to
+                manual payroll tasks, time can be focused on strategic
+                initiatives, growth, and customer satisfaction. In addition,
+                SaaS payroll services often come at a fraction of the expense of
+                hiring and maintaining an in-house payroll team, making it a
+                cost-effective solution for businesses of all sizes
+              </p>
+            </div>
+            <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
+              >
+                <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
+                <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
+                <line x1="6" y1="1" x2="6" y2="4"></line>
+                <line x1="10" y1="1" x2="10" y2="4"></line>
+                <line x1="14" y1="1" x2="14" y2="4"></line>
+              </svg>
+              <h3 className="text-xl font-semibold text-white">
+                Compliance and security
+              </h3>
+              <p className="text-gray-200">
+                Staying compliant with ever-changing payroll regulations is a
+                constant challenge. Cloud-based SaaS payroll software removes
+                this burden by keeping systems up to date with the latest legal
+                requirements. They ensure that tax calculations, deductions, and
+                reporting align with the relevant laws and regulations, reducing
+                the risk of penalties and fines. Furthermore, these services
+                prioritise data security, employing robust encryption protocols
+                and regular backups to protect sensitive employee information.
+              </p>
+            </div>
+            <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>
+              <h3 className="text-xl font-semibold text-white">
+                Scalability and flexibility
+              </h3>
+              <p className="text-gray-200">
+                As a business grows, so does the complexity of payroll
+                requirements. SaaS payroll services offer scalability and
+                flexibility, adapting to evolving needs. Whether a company has a
+                handful of employees, or a large workforce spread across
+                multiple locations, these services can accommodate payroll
+                demands seamlessly. With user-friendly interfaces and
+                customisable features, SaaS payroll solutions provide the
+                agility to handle diverse payroll scenarios.
+              </p>
+            </div>
+            <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
+              >
+                <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"></path>
+                <path d="M19 10v2a7 7 0 0 1-14 0v-2"></path>
+                <line x1="12" y1="19" x2="12" y2="23"></line>
+                <line x1="8" y1="23" x2="16" y2="23"></line>
+              </svg>
+              <h3 className="text-xl font-semibold text-white">
+                Real-time accessibility and reporting
+              </h3>
+              <p className="text-gray-200">
+                Traditional payroll systems often require manual data retrieval
+                and reporting, leading to delays and limited insights. SaaS
+                payroll services provide real-time access to payroll data,
+                empowering employers by making critical information immediately
+                available. With comprehensive reporting features, companies can
+                generate custom reports, analyse trends, and gain valuable
+                insights into payroll expenses, to ultimately facilitate
+                informed decision-making.
+              </p>
+            </div>
+            <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
+              >
+                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                <circle cx="9" cy="7" r="4"></circle>
+                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+              </svg>
+              <h3 className="text-xl font-semibold text-white">Maleability</h3>
+              <p className="text-gray-200">
+                Traditional payroll systems typically involve manual data
+                retrieval and reporting, resulting in delays and limited
+                insights. In contrast, SaaS payroll services offer real-time
+                access to payroll data, enabling employers to access crucial
+                information instantly. With robust reporting features, companies
+                can easily generate custom reports, analyze trends, and obtain
+                valuable insights into payroll expenses, ultimately supporting
+                informed decision-making.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
